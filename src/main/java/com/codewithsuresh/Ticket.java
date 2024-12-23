86d398d01af27f9407003b6e3fedebca08e9fa88
Nagulapalli Suresh <suresh.nagulapalli07@gmail.com>
2024-12-23 18:39:47 +0530

    Getter Setter 1

--- src/main/java/com/codewithsuresh/Ticket.java
+++ src/main/java/com/codewithsuresh/Ticket.java
@@ -10,8 +10,24 @@ public class Ticket{
         this.price = price;
     }
 
+    public String getTicketId(){
+        return ticketId;
+    }
 
-        public void printTicketDetails(){
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
+    public void printTicketDetails(){
             System.out.println("TicketID : " + ticketId);
             System.out.println("Fare : " + price);
         }
